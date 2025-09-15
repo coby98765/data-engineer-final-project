42068afdb9eb72a5f4d7fd3514b4e4f76c54c421
Yossi Ben Haim <v0585204770@gmail.com>
2025-09-15 11:31:34 +0300

    fixed the clean-streets and bulidings

--- src/services/expander/manager_streets.py
+++ src/services/expander/manager_streets.py
@@ -2,14 +2,18 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 
-from city_parser import CityParser
+from streets_parser import CityParser
 from src.shared.dal.kafka import Kafka
 from src.shared.dal.mongo2 import MongoDAL
+from manager_buildings import Manager_buildings
 
 
 load_dotenv()
 
 class Manager:
+
+    def __init__(self):
+        self.manager_buildings = Manager_buildings()
 
     def run(self):
         for consumer in self.kafka.sub():
@@ -20,29 +24,27 @@ class Manager:
     def setup(self):
         self.kafka = Kafka("parser-service")
         self.kafka.create_producer()
-        self.kafka.create_consumer(os.getenv("topic-to-parser", "topic-to-parser"))
+        self.kafka.create_consumer(os.getenv("topic-to-parser-streets", "topic-to-parser-streets"))
         self.mongodb = MongoDAL()
         self.parser = CityParser()
 
 
     def start_city(self, html):
-        all_city = self.parser.parse(html)
-        for row in all_city:
-            if row["status"] == "partial":
-                self.send_link_to_kafka(row.pop("link"))
-        self.saving_in_mongodb(all_city)
+        all_streets = self.parser.parse(html)
+        list_of_buildings = []
+        for street in all_streets:
+            if street["status"] == "partial":
+                list_of_buildings.append(self.manager_buildings.start_city(street, street["city"]))
+        self.saving_in_mongodb(all_streets)
 
-
-    def send_link_to_kafka(self, link):
-        self.kafka.pub(link, os.getenv("topic-to-streets", "topic-to-streets"))
 
     def saving_in_mongodb(self, doc):
-        collection = os.getenv("collection-to-doc-city")
+        collection = os.getenv("collection-to-doc-streets")
         id = self.mongodb.insert_document(collection, doc)
         self.send_id_mongo_to_geo(id)
 
     def send_id_mongo_to_geo(self, link):
-        self.kafka.producer(link, os.getenv("topic-to-geo-city", "topic-to-geo-city"))
+        self.kafka.producer(link, os.getenv("topic-to-geo-streets", "topic-to-geo-streets"))
 
 
     def get_from_mongo_by_id(self, _id):
