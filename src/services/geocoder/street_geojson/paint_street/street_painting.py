c5d10d9cb4cab2147c95fb26f73d6246cccada01
Elchanan5760 <emstone@gmail.com>
2025-09-17 15:26:04 +0300

    streets test

--- src/services/geocoder/street_geojson/paint_street/street_painting.py
+++ src/services/geocoder/street_geojson/paint_street/street_painting.py
@@ -12,22 +12,23 @@ class PaintStreet:
     def painting(self,streets_in_cities:dict):
         city_streets = gpd.GeoDataFrame()
         keys = list(streets_in_cities.keys())
-        print(streets_in_cities[keys[0]])
+        #print(streets_in_cities[keys[0]])
 
         G = ox.graph_from_place(f"{keys[0]}", network_type='drive')
         edges = ox.graph_to_gdfs(G, nodes=False, edges=True)
-        print("g:",G)
-        print("edges:",edges)
-
+        #print("g:",G)
+        #print("edges:",edges)
+        #edges.to_csv(f"{keys[0]}.csv", index=False, encoding="utf-8")
         for s in streets_in_cities[keys[0]]:
+            #print(s["street"])
             street = edges[edges["name"] == s["street"]]
-            print(street,type(street))
+            #print(street,type(street))
             if street.empty:
                 print(f"Street: {s["street"]}, not found.")
             else:
                 street['color'] = self.COLOR_MAP[s["status"]]
                 street['city'] = keys[0]
-
+                street = street.dropna(axis=1, how="all")
                 city_streets = gpd.GeoDataFrame(
                     pd.concat([city_streets, street], ignore_index=True)
                 )
