a79247acab9d97797824a99dd57a89035e0d63a2
Elchanan5760 <emstone@gmail.com>
2025-09-17 12:12:30 +0300

    add api to geocoder

--- src/services/geocoder/app.py
+++ src/services/geocoder/app.py
@@ -1,4 +1,14 @@
-from src.shared.api_template import build_service_app
-from .service import run_once
+# from src.shared.api_template import build_service_app
+from src.services.api.app import api
+from service import Operating
 
-app = build_service_app(service_name="geocoder", run_once=run_once)
+# app = build_service_app(service_name="geocoder", run_once=run_once)
+
+
+# Create FastAPI app with Manager class
+app = api(Operating, "manager geocoder")
+
+if __name__ == "__main__":
+    import uvicorn
+    # Run the FastAPI app
+    uvicorn.run(app, host="0.0.0.0", port=8006)
